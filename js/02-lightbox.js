1350f0be54846f3df1a59dc689aa6c64f63a3d98
FilipGk <skomurlight@gmail.com>
2023-02-25 10:08:03 +0100

    2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+function generateGalleryItems(items) {
+  return items
+    .map(
+      ({ preview, original, description }) => `
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}">
+      </a>
+  `
+    )
+    .join("");
+}
+
+gallery.innerHTML = generateGalleryItems(galleryItems);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
