256e76a9cd4517207ec10508e70763bc98d7df63
FilipGk <skomurlight@gmail.com>
2023-02-25 09:58:02 +0100

    1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+function generateGalleryItems(items) {
+  return items
+    .map(
+      ({ preview, original, description }) => `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
+      </a>
+    </div>
+  `
+    )
+    .join("");
+}
+
+gallery.innerHTML = generateGalleryItems(galleryItems);
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const clickedElement = event.target;
+  if (!clickedElement.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const modal = basicLightbox.create(`
+    <img src="${clickedElement.dataset.source}" alt="${clickedElement.alt}">
+  `);
+
+  modal.show();
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Escape") {
+      modal.close();
+    }
+  };
+  document.addEventListener("keydown", handleKeyDown);
+});
